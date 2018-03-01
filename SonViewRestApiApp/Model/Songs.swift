1d6254eb11efa1a652db321c97f39c94320f6a28
MacBook Pro <Hello@MacBooks-MacBook-Pro.local>
2018-03-01 19:28:39 +0600

    first commit

---
+++ SonViewRestApiApp/Model/Songs.swift
@@ -0,0 +1,9 @@
+//
+//  Songs.swift
+//  SonViewRestApiApp
+//
+//  Created by MacBook Pro on 17/2/18.
+//  Copyright © 2018 MacBook Pro. All rights reserved.
+//
+
+import Foundation
---
+++ SonViewRestApiApp/View/SongCell.swift
@@ -0,0 +1,9 @@
+//
+//  SongCell.swift
+//  SonViewRestApiApp
+//
+//  Created by MacBook Pro on 17/2/18.
+//  Copyright © 2018 MacBook Pro. All rights reserved.
+//
+
+import Foundation
